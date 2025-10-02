4ff248fbe33d083b0eaef313a1c7baa3652e31b7
enmarimo <enmarimo@gmail.com>
2025-10-02 11:13:15 +0200

    Updated copyright year. Added copyright notice to build script.

--- src/reference_viewer.lua
+++ src/reference_viewer.lua
@@ -1,6 +1,6 @@
 -- Reference image viewer extension for Aseprite.
 --
--- Copyright (c) 2024 enmarimo
+-- Copyright (c) 2025 enmarimo
 --
 -- Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 -- and associated documentation files (the “Software”), to deal in the Software without
