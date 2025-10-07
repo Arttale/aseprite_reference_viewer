2ef279034c51bbbc660efd66ce566b318da89440
enmarimo <enmarimo@gmail.com>
2025-10-07 17:10:20 +0200

    Fixed typo

--- src/reference_viewer_extension.lua
+++ src/reference_viewer_extension.lua
@@ -17,7 +17,7 @@
 -- DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 -- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-local ReferenceViewer = require("reference_viewert")
+local ReferenceViewer = require("reference_viewer")
 
 function init(plugin)
 	plugin:newMenuSeparator{
